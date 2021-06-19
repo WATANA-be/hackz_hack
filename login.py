d4a3cfbefe4071b8c7663d6b439bda20351281ee
WATANA-be <aansssr@gmail.com>
2021-06-20 00:40:54 +0900

    todo

--- login.py
+++ login.py
@@ -1,34 +1,39 @@
-import tkinter as tk
+def login():
+    import tkinter as tk
+    from tkinter import ttk
 
 
-# メインウィンドウ作成
-root = tk.Tk()
-root.title("株式会社渡邉商事")
-root.geometry("700x500")
-
-# ユーザーID
-user_label = tk.Label(text = "user")
-print()
-user_entry = tk.Entry()
-
-# パスワード
-pass_label = tk.Label(text = "password")
-print()
-pass_entry = tk.Entry()
-
-#背景の色
-root.configure(bg='gray')
+    # メインウィンドウ作成
+    root = tk.Tk()
+    root.title("security")
+    root.geometry("700x500")
 
 
-# ボタン
-button = tk.Button(text = "仕事しろ！")
+    # ユーザーID
+    user_label = tk.Label(text = "user")
+    print()
+    user_entry = tk.Entry()
 
-# ウィジェット配置
-user_label.pack()
-user_entry.pack()
-pass_label.pack()
-pass_entry.pack()
-button.pack()
+    # パスワード
+    pass_label = tk.Label(text = "password")
+    print()
+    pass_entry = tk.Entry()
 
-# 画面出力
-root.mainloop()
+    #背景の色
+    root.configure(bg='gray')
+
+
+    # ボタン
+    button = tk.Button(text = "仕事しろ！")
+
+    # ウィジェット配置
+    user_label.pack()
+    user_entry.pack()
+    pass_label.pack()
+    pass_entry.pack()
+    button.pack()
+
+    # 画面出力
+    root.mainloop()
+
+login()
